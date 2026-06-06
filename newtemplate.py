a4bbfe36f0428ebb58421bd9ff5df973c5beeb1e
Goutham-Senthil <godlygoutham@gmail.com>
2026-06-06 12:22:58 +0100

    feat: Added template files

---
+++ newtemplate.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def run_tets():
+    pass
+
+
+def main():
+    
+
+    # number of test cases
+    n = int(input())
+    for _ in range(n):
+        run_tets()
+
+if __name__ == "__main__":
+    main()
