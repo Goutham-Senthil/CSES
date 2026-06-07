5eb15d25a747b46cb74f49079e12f97cdcc428d1
Goutham-Senthil <godlygoutham@gmail.com>
2026-06-07 13:26:52 +0100

    feat: Added solution 2 for Distinct Numbers

--- Distinct Numbers/solve.py
+++ Distinct Numbers/solve.py
@@ -5,14 +5,17 @@ def run_tets():
     n = int(input())
     nums = list(map(int,input().split()))
 
+    # Set doesn't work here because
+    # there is a test case that is "anti-hashing"
+    # from "https://codeforces.com/blog/entry/122914"
+
     nums.sort()
-    count = 0
+    count = 1
     i = 0
-    while i < n:
-        while nums[i] == nums[i-1] and i+1 < n:
-            i+=1
+    for i in range(1,n):
+        if nums[i] == nums[i-1]:
+            continue
         count+=1
-        i+=1
     print(count)
 
 
