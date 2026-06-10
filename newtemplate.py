a184b9949024719893aa3a5d988f93f93004f857
Goutham-Senthil <godlygoutham@gmail.com>
2026-06-10 14:19:14 +0100

    refactor: Updated the template code
    Just rewrote the function name and the test cases variable to be grammatically correct

--- newtemplate.py
+++ newtemplate.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 
-def run_tets():
+def run_tests():
     pass
 
 
@@ -11,7 +11,7 @@ def main():
     # number of test cases
     n = int(input())
     for _ in range(n):
-        run_tets()
+        run_tests()
 
 if __name__ == "__main__":
     main()
