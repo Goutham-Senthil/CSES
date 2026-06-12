5a118826d2d3ba57c6738390650c4757b3547481
Goutham-Senthil <godlygoutham@gmail.com>
2026-06-12 20:43:57 +0100

    feat: Created a cpp template file

---
+++ ctemplate.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+void run_tests()
+{
+    ;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i = 1; i <=t; ++i)
+    {
+        run_tests();
+    }
+}
