643aa4c30148edb98f6666a9ce3984ebacc37395
Goutham-Senthil <godlygoutham@gmail.com>
2026-06-13 11:09:29 +0100

    feat: Added cpp solution

---
+++ 2167A/solve.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+// Link (https://codeforces.com/problemset/problem/2167/A)
+
+void run_tests()
+{
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+
+    if (a==b && b==c && c==d && d==a)
+    {
+        cout<<"YES\n";
+    }
+    else
+    {
+        cout<<"NO\n";
+    }
+}
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i = 1; i <=t; ++i)
+    {
+        run_tests();
+    }
+}
