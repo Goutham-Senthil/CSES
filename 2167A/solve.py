f1c809de2cd36bd1a1c26a2d74b7656a49c802c8
Goutham-Senthil <godlygoutham@gmail.com>
2026-06-13 10:59:31 +0100

    refactor: Updated the python code

--- 2167A/solve.py
+++ 2167A/solve.py
@@ -1,24 +1,22 @@
-def solve():
-    import sys
-    input = sys.stdin.readline
-    
-    # ===== WRITE YOUR SOLUTION HERE =====
-    n = int(input())
-    sqaures = []
+import sys
+input = sys.stdin.readline
 
-    for _ in range(n):
-        sqaures.append(input().split())
-    
-    for sq in sqaures:
-        if len(sq) != 4:
-            print("NO")
-        
-        elif sq[0] == sq[1] == sq[2] == sq[3]:
-            print("YES")
-        else:
-            print("NO")
+# Link (https://codeforces.com/problemset/problem/2167/A)
 
-    # ===================================
+def run_tests():
+    a,b,c,d = map(int,input().split())
+
+    if a == b == c == d:
+        print("YES")
+    else:
+        print("NO")
+
+def main():
+    
+    # number of test cases
+    t = int(input())
+    for _ in range(t):
+        run_tests()
 
 if __name__ == "__main__":
-    solve()
+    main()
